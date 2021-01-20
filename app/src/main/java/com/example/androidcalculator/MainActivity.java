334baf4ae562b5065b81daf244e21fa5a1c9ae61
Chandrarathne <GC2176@zebra.com>
2021-01-20 12:57:22 +0530

    menu bar Added

--- app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.androidcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
         et1 = findViewById(R.id.editText);
         configureSciMode();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.stand_mode:
+                Toast.makeText(this,"Standard Calculator",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.sci_mode:
+                Toast.makeText(this,"Scientific Calculator",Toast.LENGTH_SHORT).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+
+        }
+
     }
 
     private void configureSciMode() {
--- app/src/main/java/com/example/androidcalculator/SecondActivity.java
+++ app/src/main/java/com/example/androidcalculator/SecondActivity.java
@@ -1,10 +1,15 @@
 package com.example.androidcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,6 +18,29 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         configureBackButton();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.stand_mode:
+                Toast.makeText(this,"Standard Calculator",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.sci_mode:
+                Toast.makeText(this,"Scientific Calculator",Toast.LENGTH_SHORT).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+
+        }
+
     }
 
     public void configureBackButton() {
